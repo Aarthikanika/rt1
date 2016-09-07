4bcb97285132610a8f327488dc0d7b823cdcf885
Aarthikanika <aarthielangovann@gmail.com>
2016-09-07 07:37:07 +0530

    Create square.java

---
+++ square.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class gh {
+  public static void main(String[] args) {
+    Scanner s=new Scanner(System.in);
+    int[] a=new int[8];
+    for(int i=0;i<8;i++){
+    	a[i]=s.nextInt();
+    }
+    int sum1=a[0]+a[2]+a[4]+a[6];
+    int sum2=a[1]+a[3]+a[5]+a[7];
+    if (sum1==sum2){
+       System.out.print("Square");	
+    }
+    else System.out.print("Not a square");
+    
+    }
+}
