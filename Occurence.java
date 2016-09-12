248456ef2aac0fa1d96b2048657acfff9c5f6b04
Aarthikanika <aarthielangovann@gmail.com>
2016-09-12 08:00:00 +0530

    Create Occurence.java

---
+++ Occurence.java
@@ -0,0 +1,34 @@
+//input aaaaabbbbcccdef
+
+import java.util.Scanner;
+
+public class Compressing_Term {
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		sc.close();
+		String str="";
+		char[] ch = s.toCharArray();
+		int count = 1;
+		for(int i=0;i<ch.length;i++){
+			count=1;
+		for(int j=i+1;j<ch.length;j++){
+			if(ch[i]==ch[j]){
+				count = count+1;
+				ch[j]=' ';
+			}
+		}
+		if(ch[i]!=' '){
+		if(count>1){
+			str+=ch[i]+""+count;
+			//System.out.print(ch[i]+""+count);
+		}
+		else
+			str+=ch[i]+"";
+			//System.out.print(ch[i]+"");
+		}
+		}
+		System.out.println(str);
+	}
+}
